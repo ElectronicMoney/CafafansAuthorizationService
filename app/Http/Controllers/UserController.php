5e7e159e206c7d8f42bbd50b9524c75e2c8d0ecf
Emeka Augustine <cafafanscoders@gamil.com>
2019-05-13 08:06:22 -0400

    Implented CRUD on UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\User;
 use App\Traits\ApiResponser;
+
 
 class UserController extends Controller
 {
+    use ApiResponser;
     /**
      * Create a new controller instance.
      *
@@ -20,7 +25,8 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index() {
-
+        $users = User::all();
+        return $this->successResponse($users);
     }
 
     /**
@@ -30,6 +36,21 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request) {
+        //The rules
+        $rules = [
+            'name' => 'required|max:255',
+            'email' => 'required|max:255',
+        ];
+        //validate the request
+       $this->validate($request, $rules);
+        //instantiate the User
+        $user = new User();
+        $user->name    = $request->input('name');
+        $user->email  = $request->input('email');
+        //Save the user
+        $user->save();
+        //Return the new user
+        return $this->successResponse($user, Response::HTTP_CREATED);
 
     }
 
@@ -40,7 +61,8 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($userId) {
-
+        $user = User::findOrFail($userId);
+        return $this->successResponse($user);
     }
 
     /**
@@ -51,7 +73,34 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $userId) {
+        //The rules
+        $rules = [
+            'name' => 'required|max:255',
+            'email' => 'required|max:255',
+        ];
+        //validate the request
+       $this->validate($request, $rules);
+        //instantiate the User
 
+        //find the user using its id
+        $user = User::findOrFail($userId);
+        //Check if the request has name
+        if ($request->has('name')) {
+            $user->name    = $request->input('name');
+        }
+        //Check if the request has email
+        if ($request->has('email')) {
+            $user->email  = $request->input('email');
+        }
+
+        //Check if anything changed in user
+        if ($user->isClean()) {
+            return $this->errorResponse('You must specify a new value to update', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        //Save the user
+        $user->save();
+        //Return the new user
+        return $this->successResponse($user, Response::HTTP_CREATED);
     }
 
     /**
@@ -61,7 +110,11 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function destroy($userId) {
-
+        //find the user using its id
+        $user = User::findOrFail($userId);
+        $user->delete();
+        //Return the new user
+        return $this->successResponse($user);
     }
 
 }
